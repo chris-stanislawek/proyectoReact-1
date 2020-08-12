6ba1124bc967193db29722deb635fd4763e6f1a3
Chris <christophe.stanislawek@gmail.com>
2020-08-12 03:52:09 +0200

    changed navigation, changed cover worked only on CSS

--- src/Components/Coverpage.js
+++ src/Components/Coverpage.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import '../assets/styles/Components/Coverpage.scss';
-import cover from '../assets/styles/img/cover.png';
-import { Container } from 'react-bootstrap';
-
+import React from "react";
+import "../assets/styles/Components/Coverpage.scss";
+import cover from "../assets/styles/img/person.png";
+import { Container } from "react-bootstrap";
 
 const Coverpage = () => {
-    return (
-        <Container> 
-        <div className= "cover">
-            
-            <div className= "text"> 
-            <p> Find your next </p>
-            <p> REMOTE DEV JOB </p>
-            </div>
-            
-            <img  className= "cover__img" src={cover}/>
-        
-          
+  return (
+    <Container>
+      <div className="cover">
+        <div className="text">
+          <p className="mainText">FIND REMOTE</p>
+          <p className="mainText">DEV JOB IN TOP</p>
+          <p className="mainText">TECH STARTUPS</p>
+          <p>
+            We make it easy to apply to top tech companies and{" "}
+            <strong>you will always get feedback </strong>for your applications
+          </p>
+          <div className="buttonCenter">
+            <button href="#" className="coverButton">
+              Find a Job!
+            </button>
+          </div>
         </div>
-        </Container>
-    )
-}
 
-export default Coverpage
+        <img className="cover__img" src={cover} />
+      </div>
+    </Container>
+  );
+};
+
+export default Coverpage;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,33 @@
 import React from "react";
-import '../assets/styles/Components/Header.scss'
+import "../assets/styles/Components/Header.scss";
+import logo from "../assets/styles/img/unicorn_small_.png";
 
 const Header = () => {
   return (
-      <header className="header"> 
-   
-        <div className="nav-left-section"> 
-            <p> LOGO__APP </p>
-        </div>
-        
-    <ul className="nav-right-section">
-        <li> 
-            <a href="/login"> HOME </a>
+    <header className="header">
+      <div className="nav-left-section">
+        <img className="logo" src={logo} />
+      </div>
+
+      <ul className="nav-right-section">
+        <li>
+          <a href="/login"> Home </a>
         </li>
-        
-        <li> 
-         <a href="/register"> FAQ </a>
-       </li>
 
-       <li> 
-         <a href="/register"> BLOG </a>
-       </li>
+        <li>
+          <a href="/register"> FAQ </a>
+        </li>
 
-        <li> 
-       <button className="nav-right-section__button"> POST A JOB </button>
-       </li>
-      
-    </ul>
-  
-</header>
+        <li>
+          <a href="/register"> Blog </a>
+        </li>
 
+        <li>
+          <button className="nav-right-section__button"> POST A JOB </button>
+        </li>
+      </ul>
+    </header>
   );
-
 };
 
 export default Header;
