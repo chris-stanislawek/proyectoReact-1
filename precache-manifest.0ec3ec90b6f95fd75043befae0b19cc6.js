024bcc8a4b4e62d3cd06a0d959348209c841a8d7
Chris <christophe.stanislawek@gmail.com>
2020-08-15 00:00:28 +0200

    Updates

---
+++ precache-manifest.0ec3ec90b6f95fd75043befae0b19cc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65a596f865031abd9286125cb9bf925",
+    "url": "/proyectoReact/index.html"
+  },
+  {
+    "revision": "04a8dd28a73c6c83ea1d",
+    "url": "/proyectoReact/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "86485241ddd9ffd4faf9",
+    "url": "/proyectoReact/static/css/main.8738e1fe.chunk.css"
+  },
+  {
+    "revision": "04a8dd28a73c6c83ea1d",
+    "url": "/proyectoReact/static/js/2.1a36932d.chunk.js"
+  },
+  {
+    "revision": "f4af03c0f2908a2649cff08bfcaee501",
+    "url": "/proyectoReact/static/js/2.1a36932d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86485241ddd9ffd4faf9",
+    "url": "/proyectoReact/static/js/main.bc0eac2a.chunk.js"
+  },
+  {
+    "revision": "ebeabf0b1e228daa9acf",
+    "url": "/proyectoReact/static/js/runtime-main.b16999b4.js"
+  },
+  {
+    "revision": "60e4898fa6c47e9980060c1d598f1227",
+    "url": "/proyectoReact/static/media/person.60e4898f.png"
+  }
+]);
