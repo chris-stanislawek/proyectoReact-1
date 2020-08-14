d2931684b13bfda352cd16117d494df8a3a8226b
Karla Mora <karlamorasg@gmail.com>
2020-08-14 03:07:59 +0200

    solucion hooks

--- src/Hooks/UseFetchJobs.js
+++ src/Hooks/UseFetchJobs.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-import { useReducer, useEffect } from "react";
+import { useReducer, useEffect, useState } from "react";
 import axios from "axios";
-
 const ACTIONS = {
   MAKE_REQUEST: "make-requests",
   GET_DATA: "get-data",
   ERROR: "error",
 };
-
 const BASE_URL =
   "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?location=remote";
-
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
@@ -27,22 +23,17 @@ function reducer(state, action) {
     default:
       return state;
   }
-=======
-
-import { useReducer, useEffect, useState } from 'react';
-import axios from 'axios';
-
-
-const ACTIONS= {
-    MAKE_REQUEST: 'make-requests',
-    GET_DATA: 'get-data',
-    ERROR: 'error'
->>>>>>> 30be171bcb4871f29ca1fb3b074d43a23783c16d
 }
-
 export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
-
+  function getJob(tecnology) {
+    tecnology = "react";
+    const URL = `https://jobs.github.com/positions.json?location=remote&description=${tecnology}`;
+    axios
+      .get(URL)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }
   useEffect(() => {
     const cancelToken = axios.CancelToken.source();
     dispatch({ type: ACTIONS.MAKE_REQUEST });
@@ -58,60 +49,9 @@ export default function useFetchJobs(params, page) {
         if (axios.isCancel(e)) return;
         dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
       });
-
     return () => {
       cancelToken.cancel();
     };
   }, [params, page]);
-
   return state;
 }
-<<<<<<< HEAD
-=======
-
-export default function useFetchJobs(params, page){
-    const [state, dispatch] = useReducer(reducer,{jobs:[], loading: true})
-
-    
-
-    function getJob (tecnology) {
-
-         tecnology = 'react'
-         const URL = `https://jobs.github.com/positions.json?location=remote&description=${tecnology}`
-
-         axios.get(URL)
-            .then( response => console.log(response.data))
-            .catch(error => console.log(error))
-    }
-
-   
-
-    
-
-
-
-   useEffect(() => {
-       const cancelToken = axios.CancelToken.source()
-       dispatch({type: ACTIONS.MAKE_REQUEST })
-       axios.get(BASE_URL,{
-           cancelToken: cancelToken.token,
-           params: {markdow: true, page: page , ...params}
-       })
-       .then(res => {
-           dispatch({type: ACTIONS.GET_DATA, payload: {jobs:res.data}})
-       })
-       .catch(e =>{
-           if(axios.isCancel(e)) return
-        dispatch({type: ACTIONS.ERROR, payload: {error:e}})
-       })
-    
-       return()=>{
-           cancelToken.cancel()
-       }
-   }, [params,page]);
-
-    return state
-    
-
-}
->>>>>>> 30be171bcb4871f29ca1fb3b074d43a23783c16d
